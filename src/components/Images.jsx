98e0b3c0cea83b8e29cc8e6161802eab6e966d54
rishu2604 <anand.rishu310@gmail.com>
2024-02-20 11:51:48 +0530

    "Fixed Bugs"

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -5,7 +5,7 @@ export default function Images({ clickHandler, imagesPaths }) {
     return (
         <div className='icon'>
             {imagesPaths.map((img, index)=>(
-                <button key={index} onClick={() => clickHandler(index)}>
+                <button key={img.name} onClick={() => clickHandler(img.name)}>
                     <img src={img.src} alt={`pokemon${index}`} />
                     <strong>{img.name}</strong>
                 </button>
