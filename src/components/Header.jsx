bc4ca9f2aa2289b6b8e425d2bd36011b224b02a0
rishu2604 <anand.rishu310@gmail.com>
2024-02-20 11:41:28 +0530

    "Updated styles"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ import ScoreCard from './ScoreCard';
 
 export default function Header({score, highScore}) {
     return (
-        <>
+        <div className='main'>
             <div className='main-text'>
-                <p>Amphibia Memory game</p>
+                <p>Amphibia Memory Game</p>
                 <ScoreCard score={score} highScore={highScore}/>
             </div>
-            <p>Get points by clicking on an image but don't click on any more than once!</p>
-            <p>Clicking an image more than once will reset your score!</p>
-        </> 
+            <p className='ptag'>Get points by clicking on an image but don't click on any more than once!</p>
+            <p className='ptag'>Clicking an image more than once will reset your score!</p>
+        </div> 
     );
 }
